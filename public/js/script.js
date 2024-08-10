3e3359fafc4febc29a00ace25ae118d4d97df397
FSGemini <artezyfahmi@gmail.com>
2024-08-10 12:07:58 +0700

    new upgrade

--- public/js/script.js
+++ public/js/script.js
@@ -1,49 +1,80 @@
-// ketika tombol menu diklik
-const menu = document.querySelector('#hamburgerMenu');
+const hamburgerMenu = document.getElementById('hamburger_menu');
+const bars = document.querySelectorAll('.hamburger-bar');
 const navbar = document.getElementById('navbar');
-const barMenu = document.querySelectorAll('.hamburger-bar');
-const navLinks = document.querySelectorAll('nav a');
+const links = document.querySelectorAll('.nav-link');
 
-menu.addEventListener('click', () => {
-  navbar.classList.toggle('right-0');
-
-  barMenu.forEach((bar) => {
+hamburgerMenu.addEventListener('click', () => {
+  bars.forEach((bar) => {
     bar.classList.toggle('barClose');
   });
+  navbar.classList.toggle('scale-y-0');
 });
 
-navLinks.forEach((link) => {
+links.forEach((link) => {
   link.addEventListener('click', () => {
-    navbar.classList.toggle('right-0');
-
-    barMenu.forEach((bar) => {
+    bars.forEach((bar) => {
       bar.classList.toggle('barClose');
     });
+    navbar.classList.toggle('scale-y-0');
   });
 });
 
-document.addEventListener('click', (e) => {
-  if (!navbar.contains(e.target) && !menu.contains(e.target)) {
-    navbar.classList.remove('right-0');
-    barMenu.forEach((bar) => {
-      bar.classList.remove('barClose');
-    });
-  }
-});
+// Modal Box
+const btnOpen = document.querySelectorAll('.btn__explain');
 
-// ketika tombol lihat penjelasan diklik maka akan menampilkan box modal
-const btnExplainBeans = document.querySelectorAll('.btn__explain');
-const modal = document.querySelectorAll('.modal__beans');
-const closeModal = document.querySelector('.fa-close');
-
-btnExplainBeans.forEach((btn) => {
+btnOpen.forEach((btn) => {
   btn.addEventListener('click', () => {
     const id = btn.getAttribute('data-content');
-    const modalId = document.getElementById(id);
-    modalId.classList.toggle('hidden');
+    const modal = document.getElementById(id);
+    modal.style.display = 'flex';
   });
 });
 
+const modalBeans = document.querySelectorAll('.modal__beans');
 function closeModal() {
-  modal.classList.toggle('hidden');
+  modalBeans.forEach((modal) => {
+    modal.style.display = 'none';
+  });
 }
+
+modalBeans.forEach((modal) => {
+  modal.addEventListener('click', (e) => {
+    if (e.target === modal) closeModal();
+  });
+});
+
+const cardRecept = document.querySelectorAll('.card__recept');
+
+cardRecept.forEach((card) => {
+  card.addEventListener('click', () => {
+    const id = card.getAttribute('data-recept');
+    const modal = document.getElementById(id);
+    modal.classList.add('left-0');
+    modal.classList.remove('-left-full');
+  });
+});
+
+const contentRecept = document.querySelectorAll('.content__recept');
+function closeBoxRecept() {
+  contentRecept.forEach((content) => {
+    content.classList.add('-left-full');
+    content.classList.remove('left-0');
+  });
+}
+
+contentRecept.forEach((content) => {
+  content.addEventListener('click', (e) => {
+    if (!e.target.matches('.content__recept *')) {
+      content.classList.add('-left-full');
+    }
+  });
+});
+
+window.addEventListener('click', (event) => {
+  if (!navbar.contains(event.target) && !hamburgerMenu.contains(event.target)) {
+    bars.forEach((bar) => {
+      bar.classList.remove('barClose');
+    });
+    navbar.classList.add('scale-y-0');
+  }
+});
